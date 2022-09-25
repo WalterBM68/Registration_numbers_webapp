a190d0bfa61d0d3fbbeae1c3aac194639ccf2c1f
WalterBM68 <Wbmanjanja@gmail.com>
2022-09-25 13:34:55 +0200

    error messages has been fixed

--- routes.js
+++ routes.js
@@ -1,15 +1,38 @@
+const express = require('express');
+const app = express();
+const flash = require('express-flash');
+const session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
+
 module.exports = Routes = (regNumbers, regNumberTable) => {
     //Home route
     const homeRoute = async (req, res) => {
+        const registrations = await regNumberTable.displayMyRegs();
         res.render('index',{
-         dispregs: await regNumberTable.displayMyRegs()
+            registrations 
         });
     }
     //displaying reg numbers & filter
     const displayRegNumbers = async (req, res) => {
         const name = req.body.regNumeber;
         const town = req.body.town;
-        await regNumberTable.storeRegNumbers(name);
+        let capitalizeLetters = name.toUpperCase();
+        let formatRegNumber1 = /^[CY|CJ|CA|CF]{2}\s[0-9]{3}\-[0-9]{3}$/;
+        let formatRegNumber2 = /^[CY|CJ|CA|CF]{2}\s[0-9]{6}$/;
+        let formatRegNumber3 = /^[CY|CJ|CA|CF]{2}\s[0-9]{4}$/;
+        let formatRegNumber4 = /^[CY|CJ|CA|CF]{2}\s[0-9]{3}\s[0-9]{3}$/;
+        if(formatRegNumber1.test(capitalizeLetters) == true || formatRegNumber2.test(capitalizeLetters) == true ||
+        formatRegNumber3.test(capitalizeLetters) == true || formatRegNumber4.test(capitalizeLetters) == true){
+            await regNumberTable.storeRegNumbers(name);
+        }else if(formatRegNumber1.test(capitalizeLetters) !== true || formatRegNumber2.test(capitalizeLetters) !== true ||
+        formatRegNumber3.test(capitalizeLetters) !== true || formatRegNumber4.test(capitalizeLetters) !== true){
+            req.flash('info', 'Please enter a valid registration number');
+        }
         await regNumberTable.getRegNumbers()
         await regNumberTable.filterTowns(town)
         res.redirect('/');
@@ -18,6 +41,7 @@ module.exports = Routes = (regNumbers, regNumberTable) => {
     const clearAllRegNumbers = async (req, res) => {
         await regNumberTable.deleteRegNumbers();
         await regNumberTable.getRegNumbers()
+        req.flash('info', 'Registration numbers have been deleted');
         res.redirect('/');
     }
     return{
